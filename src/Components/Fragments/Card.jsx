99317b8ec8990e38cff29409ec9587fdb5b8005f
Rifqi Dani Putranto <rifqidani233@gmail.com>
2024-09-09 23:39:02 +0700

    Rendering List

--- src/Components/Fragments/Card.jsx
+++ src/Components/Fragments/Card.jsx
@@ -5,7 +5,7 @@ const CardProduct = (props) => {
  
     const {children} = props
     return (
-        <div className=" mx-2 w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow ">
+        <div className=" mx-2 w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow flex flex-col justify-between ">
        
      {children}
       
@@ -19,9 +19,9 @@ const Body = (props) => {
     const {children, title} = props
     
     return(
-        <div className="px-5 pb-5">
+        <div className="px-5 pb-5 h-full">
         <a href="#">
-            <h5 className="text-xls font-semibold tracking-tight text-white">{title}</h5>
+            <h5 className="text-xl font-semibold tracking-tight text-white ">{title}</h5>
             <p className="text-s text-white">{children}</p>
         </a>
     </div>
--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,33 +1,52 @@
+/* eslint-disable react/jsx-key */
 
 import CardProduct from "../Components/Fragments/Card"
+
+
+const oshi = [
+    {
+        id:1,
+        image : "/teresa.jpg",
+        title : "IKEDA TERESA",
+        description : "No.1 Best Girl",
+        price : "Priceless"
+    },
+    {
+        id:2,
+        image : "/ayame.jpg",
+        title : "Ayame shan",
+        description : "Cutest Girl",
+        price : "Priceless"
+    },
+    {
+        id:3,
+        image : "/shizuki.jpeg",
+        title : "Shiizuki",
+        description : "shy girl",
+        price : "Priceless"
+    }
+]
 
 const ProductPage = () => {
     return (
         <div className="flex justify-center py-5">
- <CardProduct>
-<CardProduct.Header image="/ayame.jpg"/>
 
- <CardProduct.Body title="AYAME SHANNN">
-    Cutest Girl EVER
- </CardProduct.Body>
 
- <CardProduct.Footer price="Priceless"/>
-
- </CardProduct>
-
+ {oshi.map((oshi)=>(
 
  <CardProduct>
-<CardProduct.Header image="/teresa.jpg"/>
-
- <CardProduct.Body title="IKEDA TERESA">
-    No.1 Best Girl
- </CardProduct.Body>
-
- <CardProduct.Footer price="Priceless"/>
-
- </CardProduct>
+ <CardProduct.Header image={oshi.image}/>
+ 
+  <CardProduct.Body title={oshi.title}>
+     {oshi.description}
+  </CardProduct.Body>
+ 
+  <CardProduct.Footer price={oshi.price}/>
+ 
+  </CardProduct>
+ ))}
         </div>
-    )
-}
+    );
+};
 
 export default ProductPage
